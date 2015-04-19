48fc26dd1d01fce0e43c9ef47b8c6e3d49aa9680
Boris V <boris@codechimp.io>
2015-04-19 18:19:41 +0100

    * Add simple example

---
+++ example/simple.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bgv/redisurl"
+)
+
+func main() {
+	// Parse redis url
+	url, err := redisurl.Parse("redis://:VerySecret@127.0.0.1:6379/5")
+
+    // Check for error
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Got Redis Info - Host: %s, Port: %s, Password: %s, Db: %s\n", url.Host, url.Port, url.Pass, url.Db))
+}
